4022b1e260188557ff9e5c36a93d1de669d617db
Reuben Tier <otterlord.dev@gmail.com>
2024-10-19 17:00:27 +0100

    stuff

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Install', slug: 'guides/install' },
 					],
 				},
 				{
--- apps/docs/src/content/docs/guides/install.md
+++ apps/docs/src/content/docs/guides/install.md
@@ -1,6 +1,6 @@
 ---
-title: Example Guide
-description: A guide in my new Starlight docs site.
+title: Install Theme
+description: Install the Catppuccin theme for Starlight.
 ---
 
 Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
